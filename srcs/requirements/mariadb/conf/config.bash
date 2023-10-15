e2d1a7a2d43d51cbbf6a531dd94505399bbc8a3f
Anas jaidi <anas.jaidi@icloud.com>
2023-10-15 12:02:04 +0100

    feat: finish mariadb container

--- srcs/requirements/mariadb/conf/config.bash
+++ srcs/requirements/mariadb/conf/config.bash
@@ -8,23 +8,22 @@ handle_error() {
   exit ${exit_code}
 }
 
-# Update repositories
 apt update -y
 if [ $? -ne 0 ]; then
   handle_error 1 "apt update -y"
 fi
-
+apt upgrade -y
+if [ $? -ne 0 ]; then
+  handle_error 1 "apt update -y"
+fi
 # Install MariaDB server
 apt install mariadb-server -y
 if [ $? -ne 0 ]; then
   handle_error 1 "apt install mariadb-server"
 fi
 
-# Start MariaDB service
-service mariadb start
-if [ $? -ne 0 ]; then
-  handle_error 1 "service mariadb start"
-fi
+sed -i "s/^bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
+
 
 # Check if required environment variables are set
 if [ -z "$DB_USER" ] || [ -z "$DB_NAME" ] || [ -z "$DB_PASS" ]; then
@@ -32,31 +31,25 @@ if [ -z "$DB_USER" ] || [ -z "$DB_NAME" ] || [ -z "$DB_PASS" ]; then
 fi
 
 # Generate seed file with variable substitution
-path_conf="/etc/mysql/mariadb.conf.d"
-filename="50-server.cnf"
 sed "s/%%DB_NAME%%/$DB_NAME/g; s/%%DB_USER%%/$DB_USER/g; s/%%DB_PASS%%/$DB_PASS/g" template.sql >mariadb_seed.sql
 if [ $? -ne 0 ]; then
   handle_error 1 "sed"
 fi
 
-# Copy configuration file to destination
-cp mariadb.conf "$path_conf/$filename"
+
+# Start MariaDB service
+service mariadb start
 if [ $? -ne 0 ]; then
-  handle_error 1 "config of mariadb server fialed"
+  handle_error 1 "service mariadb start"
 fi
 
-# Seed the database
-mysql <mariadb_seed.sql
+
+# # Seed the database
+mysql  <mariadb_seed.sql
 if [ $? -ne 0 ]; then
   handle_error 1 "seeding mariadb user and db failed"
 fi
 
-
-# Start MariaDB server
-# exec mysqld --user=root --console
-
-mariadb
-
-if [ $? -ne 0 ]; then
-  handle_error 1 "mysql -d"
-fi
+kill `cat /var/run/mysqld/mysqld.pid`
+service mariadb stop
+mysqld
